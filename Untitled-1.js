bf25780a79034ae2fc53643060d0788535962ecf
isaacbracho <isaacbracho676@gmail.com>
2025-07-30 12:16:09 -0400

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,8 @@
+let nombre = "isaac";
+const ciudad = "ZULIA";
+var edad = 17;
+
+
+console.log("Hola mi nombre es "+ nombre + ".");
+console.log("Vivo en la ciudad " + ciudad + ".");
+console.log("Y mi edad es "+ edad + "años.");
---
+++ let arma = 0;.js
@@ -0,0 +1,5 @@
+let arma = 4;
+let base = 4;
+let multiplicacion=arma * base
+
+console.log(`Multiplicacion: ${multiplicacion}`);
